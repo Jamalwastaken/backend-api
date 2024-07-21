92ee4f6e75ff8e7ecefe60b497f6872fe8cf2509
Jamalwastaken <aehiede13@gmail.com>
2024-07-21 16:36:03 +0100

    added post a new task and get task by id

--- swagger_server/controllers/task_controller.py
+++ swagger_server/controllers/task_controller.py
@@ -1,10 +1,9 @@
 import connexion
-import six
+from flask import jsonify, abort
 
-from swagger_server.models.collaborator_inner import CollaboratorInner  # noqa: E501
-from swagger_server.models.task import Task  # noqa: E501
-from swagger_server.models.user import User  # noqa: E501
-from swagger_server import util
+from swagger_server.controllers.entities import Task
+from swagger_server.db import db
+from swagger_server.models import CollaboratorInner
 
 
 def add_collaborator(body, task_id):  # noqa: E501
@@ -34,37 +33,20 @@ def add_task(body):  # noqa: E501
 
     :rtype: Task
     """
-    if connexion.request.is_json:
-        body = Task.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    try:
+        if connexion.request.is_json:
+            d = connexion.request.get_json()
+            if "task_id" in d.keys():
+                d.pop("task_id")
+
+            task = Task(**d)
+            db.session.add(task)
+            db.session.commit()
+            return jsonify(task)
+    except Exception as e:
+        abort(400, str(e))
 
 
-def add_task(task_id, title, description, start_date, end_date, completed_at, author_id):  # noqa: E501
-    """Add a task
-
-    Add a task # noqa: E501
-
-    :param task_id: 
-    :type task_id: int
-    :param title: 
-    :type title: str
-    :param description: 
-    :type description: str
-    :param start_date: 
-    :type start_date: str
-    :param end_date: 
-    :type end_date: str
-    :param completed_at: 
-    :type completed_at: str
-    :param author_id: 
-    :type author_id: int
-
-    :rtype: Task
-    """
-    start_date = util.deserialize_datetime(start_date)
-    end_date = util.deserialize_datetime(end_date)
-    completed_at = util.deserialize_datetime(completed_at)
-    return 'do some magic!'
 
 
 def delete_collaborator(task_id, user_id):  # noqa: E501
@@ -118,7 +100,10 @@ def get_task_by_id(task_id):  # noqa: E501
 
     :rtype: Task
     """
-    return 'do some magic!'
+    task = Task.query.filter_by(task_id=task_id).first()
+    if not task:
+        abort(404, f"{task_id} not found")
+    return jsonify(task)
 
 
 def get_task_collaborators(task_id):  # noqa: E501
