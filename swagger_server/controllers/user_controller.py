69f1e393da9740eef7ec0d06554801559710253b
Jamalwastaken <aehiede13@gmail.com>
2024-07-21 16:20:18 +0100

    added update and delete user

--- swagger_server/controllers/user_controller.py
+++ swagger_server/controllers/user_controller.py
@@ -20,7 +20,6 @@ def create_user(body=None):  # noqa: E501
     """
     try:
         if connexion.request.is_json:
-            #body = User.from_dict(connexion.request.get_json())  # noqa: E501
             d = connexion.request.get_json()
             if "user_id" in d.keys():
                 d.pop("user_id")
@@ -42,8 +41,10 @@ def delete_user(username):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
-
+    delete_count = User.query.filter_by(username=username).delete()
+    db.session.commit()
+    if delete_count == 0:
+        abort(404, f"{username} not found")
 
 def get_user_by_name(username):  # noqa: E501
     """Get user by user name
@@ -99,27 +100,21 @@ def update_user(username, body=None):  # noqa: E501
 
     :rtype: None
     """
-    if connexion.request.is_json:
-        body = User.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    try:
+        if connexion.request.is_json:
+            d = connexion.request.get_json()
+            if "user_id" in d.keys():
+                d.pop("user_id")
 
+            user = User.query.filter_by(username=username).first()
+            if user :
+                for key, value in d.items():
+                    setattr(user, key, value)
+                db.session.add(user)
+                db.session.commit()
+                return jsonify(user)
+    except Exception as e:
+        abort(400, str(e))
 
-def update_user(username, user_id=None, username2=None, email=None, password=None):  # noqa: E501
-    """Update user
+    abort(404, f"{username} not found")
 
-    This can only be done by the logged in user. # noqa: E501
-
-    :param username: name that need to be deleted
-    :type username: str
-    :param user_id: 
-    :type user_id: int
-    :param username2: 
-    :type username2: str
-    :param email: 
-    :type email: str
-    :param password: 
-    :type password: str
-
-    :rtype: None
-    """
-    return 'do some magic!'
